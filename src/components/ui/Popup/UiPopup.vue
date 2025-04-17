2ea11b018ef9c43b09838a9d13a61488a6a4a660
mostovoy <mostovoy@uteka.ru>
2025-04-17 18:35:59 +0300

    WIP

---
+++ src/components/ui/Popup/UiPopup.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+/**
+ * Props:
+ * title: string,
+ * open: boolean,
+ * hasClose?: boolean,
+ */
+
+/**
+ * Emits:
+ * update:open: (value: boolean),
+ */
+</script>
+
+<template>
+
+</template>
+
+<style src="./UiPopup.css" scoped></style>
